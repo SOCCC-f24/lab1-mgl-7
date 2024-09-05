dbd004dba091f1462a4f1ca284a59ad66064a3e4
mgl-7 <mgl7@sunyorange.edu>
2024-09-05 19:50:06 -0400

    Update temp_converter.py
    added custom input and Pemdas

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -2,11 +2,11 @@
 
 # process
 def c2f(c):
-    return c * 9 / 5 + 32
+    return ((c*9)/5)+32 #PEMDAS
 
 def main(cel):
     return c2f(cel)
 
 if __name__ == "__main__":
-    cel = 100         # input
+    cel = int(input("What do you want Celsius to be? "))        # input and Chanes to interger
     print(main(cel))  # output
